8296496e9cee810913af6afd247c3a1472475b60
Maria Cândida <79120586+m7c4nd1d4@users.noreply.github.com>
2022-05-13 21:08:29 +0000

    adicionando bem vindo

---
+++ aula_0513/Bemvindo.cs
@@ -0,0 +1,8 @@
+using System;
+class Progam {
+  public static void Main() {
+  Console.WriteLine("Digite seu nome");
+  string s = Console.ReadLine();
+  Console.WriteLine("Bem vindo, " + s);
+    }
+ }
--- aula_0513/OlaMundo.cs
+++ aula_0513/OlaMundo.cs
@@ -1,7 +1,7 @@
 using System;
 class Progam {
   public static void Main() {
-  Console.writeline("Olá mundo");
+  Console.WriteLine("Olá mundo");
    }
  }
  
