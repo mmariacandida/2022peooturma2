4ef8cec7e1915fdc2da1b3c80e1c62b9bc94b768
Maria Cândida <79120586+m7c4nd1d4@users.noreply.github.com>
2022-05-20 20:53:30 +0000

    tarefa

---
+++ aula_0520/ex1036.cs
@@ -0,0 +1,11 @@
+using System;
+class Program{
+  public static void Main(){
+    Console.WriteLine("Digite agora")
+      string[] X = Console.ReadLine().Split();
+      double a = double.Parse(X[0]);
+      double b = double
+      
+    }
+    
+}
