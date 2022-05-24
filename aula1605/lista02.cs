18908cb38bc174295561172490ded8b8230e2c52
Maria Cândida <79120586+m7c4nd1d4@users.noreply.github.com>
2022-05-24 01:40:45 +0000

    listas1a4

---
+++ aula1605/lista02.cs
@@ -0,0 +1,27 @@
+using System;
+class Program {
+	public static void Main () {
+		Console.WriteLine("Digite Seu Primeiro Nome:");
+		string x = Console.ReadLine();
+		Console.WriteLine($"Bem Vindo ao C#, {x}");
+    Console.WriteLine("-----------------------");
+
+		Console.WriteLine("Digite seu nome completo:");
+		string y = Console.ReadLine();
+		string[] v = y.Split();
+		Console.WriteLine($"Bem Vindo ao C#, {v[0]}");
+		Console.WriteLine("-----------------------");
+
+		Console.WriteLine("Digite a nota do primeiro bimestre da diciplina");
+		int i = int.Parse(Console.ReadLine());
+		Console.WriteLine("Digite a nota do segundo bimestre da diciplina");
+		int k = int.Parse(Console.ReadLine());
+
+		int m = (i * 2);
+		int n = (k * 3);
+		int s = (i + k / 2);
+			
+		Console.WriteLine($"Media parcial = {s}");
+
+	}
+}
--- aula_0520/ex1036.cs
+++ aula_0520/ex1036.cs
@@ -1,11 +1,24 @@
 using System;
 class Program{
   public static void Main(){
-    Console.WriteLine("Digite agora")
+   // Console.WriteLine("Digite agora");
       string[] X = Console.ReadLine().Split();
+    
       double a = double.Parse(X[0]);
-      double b = double
-      
+      double b = double.Parse(X[1]);
+      double c = double.Parse(X[2]);
+    
+      double delta = Math.Sqrt((b * b) - 4 * a * c);
+      if (delta < 0 || a == 0) Console.WriteLine("Impossivel calcular");
+      else {
+    
+      double R1 = (-b + delta) / (2 * a);
+      double R2 = (-b - delta) / (2 * a);
+
+      Console.WriteLine($"R1 = {R1:f5}");
+      Console.WriteLine($"R2 = {R2:f5}");
+    
+      }
     }
     
 }
